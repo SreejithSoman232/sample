4fed414f47db76a74bbbac9bc669f345d5dd8af7
Sreejith Soman <106295626+SreejithSoman232@users.noreply.github.com>
2022-07-25 11:37:38 +0530

    Add files via upload

---
+++ package.sql
@@ -0,0 +1,18 @@
+/*
+SQLyog Community v13.1.7 (64 bit)
+MySQL - 10.4.24-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `package` (
+	`id` int (11),
+	`packagename` varchar (765),
+	`packagetype` varchar (765),
+	`location` varchar (765),
+	`price` int (11),
+	`feature` varchar (765),
+	`details` varchar (765),
+	`packageimage` blob 
+); 
+insert into `package` (`id`, `packagename`, `packagetype`, `location`, `price`, `feature`, `details`, `packageimage`) values('1','svadva','sbfssbf','fbnsfns','5373','cbsfbs','sfbsb','Screenshot (5).png');
